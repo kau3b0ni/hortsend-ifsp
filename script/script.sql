f9a800aae0a16e961a982740cec4665a4108384d
Junioredua <junioredua@gmail.com>
2017-09-20 09:42:55 -0300

    Mudanças Script
    Mudanças no script.

--- script/script.sql
+++ script/script.sql
@@ -3,30 +3,30 @@ create database hortsend;
 use hortsend;
 
 create table usuario(
-	id int not null primary key auto_increment,
+    id int not null primary key auto_increment,
     email varchar(50) not null unique,
     senha varchar(30) not null,
     nivel_acesso int not null
 );
 
 create table administrador(
-	matricula int not null primary key auto_increment,
+    matricula int not null primary key auto_increment,
     nome varchar(50) not null,
-	usuario_id int not null,
+    usuario_id int not null,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
 create table cliente(
-	id_cliente int not null primary key auto_increment,
+    id_cliente int not null primary key auto_increment,
     nome_cliente varchar(50) not null,
     cpf_cliente varchar(11) not null,
-	telefone_contato varchar(15) not null,
-	usuario_id int not null,
+    telefone_contato varchar(15) not null,
+    usuario_id int not null,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
 create table endereco(
-	id_endereco int not null primary key auto_increment,
+    id_endereco int not null primary key auto_increment,
     id_cliente int not null,
     rua varchar(50),
     numero varchar(50),
@@ -38,17 +38,17 @@ create table endereco(
 );
 
 create table fornecedor(
-	id_fornecedor int not null primary key auto_increment,
+    id_fornecedor int not null primary key auto_increment,
     razaosocial_fornecedor varchar(50) not null,
     cpnj_fornecedor varchar(14),
     telefone_contato varchar(15),
-	usuario_id int not null,
+    usuario_id int not null,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
 
 create table produto(
-	id_produto int not null primary key auto_increment,
-	nome_produto varchar(50),
+    id_produto int not null primary key auto_increment,
+    nome_produto varchar(50),
     demanda_geral int,
     unidade varchar(10),
     preco_custo float,
@@ -56,7 +56,7 @@ create table produto(
 );
 
 create table produto_fornecedor(
-	id int not null primary key auto_increment,
+    id int not null primary key auto_increment,
     id_produto int not null,
     id_fornecedor int not null,
     quantidade_demanda int,
@@ -70,10 +70,10 @@ create table percentual(
 );
 
 create table percentual_auditoria(
-	id int not null primary key auto_increment,
+    id int not null primary key auto_increment,
     data_modificacao date,
     percentual_old float,
-	administrador_matricula int,
+    administrador_matricula int,
     FOREIGN KEY (administrador_matricula) REFERENCES administrador(matricula)
 );
 
